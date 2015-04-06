5453e859bc131c98d7bdf8d13c66ac05cceda79c
anuapama kattiparambil prakasan <anupama@localhost.localdomain>
2015-04-06 22:58:12 +0200

    requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 setup(name='auto_twitter',
-	version='0.0.1',
-	packages=find_packages(),
-	package_data = {
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.json'],
-    },
+      version='0.0.1',
+      packages=find_packages(),
+      package_data = {
+          # If any package contains *.txt or *.rst files, include them:
+          '': ['*.json'],
 
-
-)
+          },
+      install_requires=['robotframework-selenium2library','robotframework-faker'],
+      )
